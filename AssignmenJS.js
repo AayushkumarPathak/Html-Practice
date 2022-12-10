3baf9854e7ba8924d9b83ff616838f4cedb33b25
AayushPathak <78433811+AayushkumarPathak@users.noreply.github.com>
2022-12-10 11:31:47 +0530

    Add files via upload

---
+++ AssignmenJS.js
@@ -0,0 +1,14 @@
+let n = 5;
+let string = "";
+for (let i = 1; i <= n; i++) {
+  
+  for (let j = 0; j < n - i; j++) {
+    string += " ";
+  }
+  
+  for (let k = 0; k < i; k++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
