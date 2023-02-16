ab5542a783dfd6e89fa2a902899785a0a91a80f7
AayushPathak <78433811+AayushkumarPathak@users.noreply.github.com>
2023-02-16 15:26:31 +0530

    Add files via upload

---
+++ Prog2_KOC09_68.js
@@ -0,0 +1,11 @@
+
+let r  = parseInt(prompt("Enter the Radius of circle"));
+
+function areaCircle(r){
+    let area = 3.14*(r)**2;
+    alert("Area of circle is:"  +   " " +   area);
+ }
+
+ areaCircle(r);
+
+
